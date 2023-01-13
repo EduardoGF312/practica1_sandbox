e9cb4bdd70e91ed88f183b2808526c2b75004943
EduardoGF312 <118926637+EduardoGF312@users.noreply.github.com>
2023-01-13 14:49:09 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>MI PRACTICA CON CODESANDBOX</h1>
+<h2>EDUARDO GARCIA FLORES</h2>
+<div>
+  UNIVERSIDAD TECNOLOGICA DE DURANGO
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">MI GITHUB</a>.
+</div>
+`;
